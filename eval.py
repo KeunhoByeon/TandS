3a3dc946a517f386010f4d50451294a63f4b2d2b
KeunhoByeon <bkh5922a@gmail.com>
2022-06-29 18:01:28 +0900

    Fix eval debugging

--- eval.py
+++ eval.py
@@ -21,8 +21,8 @@ def blending(img, mask):
     img_g = img[:, :, 1]
     img_r = img[:, :, 2]
 
-    img_b = np.where(mask == 127, img_b * 2 + 127, img_b)
-    img_g = np.where(mask > 127, img_g * 2 + 127, img_g)
+    img_b = np.where(mask == 1, img_b * 2 + 127, img_b)
+    img_g = np.where(mask == 2, img_g * 2 + 127, img_g)
 
     output = np.zeros((img.shape[0], img.shape[1], 3))
     output[:, :, 0] = img_b
@@ -67,14 +67,15 @@ def eval(args):
             mask = output[0].detach().numpy().argmax(axis=1)[0]
             if np.max(mask) == 0:
                 continue
-            cv2.imwrite(os.path.join(args.save_dir, os.path.basename(file_path)), blending(input_np, mask))
+            # cv2.imwrite(os.path.join(args.save_dir, os.path.basename(file_path)), blending(input_np, mask))
+            cv2.imwrite(os.path.join(args.save_dir, os.path.basename(file_path)), mask)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PyTorch Training')
     parser.add_argument('--use_model', default='B')
     parser.add_argument('--data', default='~/data/bkkang/i2i/val/hrt2/')
-    parser.add_argument('--resume', default='./results/20220613171535/models/31.pth', type=str, help='path to latest checkpoint')
+    parser.add_argument('--resume', default='./results/20220621172027/models/80.pth', type=str, help='path to latest checkpoint')
     parser.add_argument('--save_dir', default='./results/temp_eval', type=str)
     parser.add_argument('--cuda', default=False)
     args = parser.parse_args()
